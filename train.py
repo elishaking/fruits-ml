d3d8961b0dbc3281a3634043164164353598ebd2
elishaking <ek.chibueze@gmail.com>
2018-06-10 03:14:33 +0100

    train

--- train.py
+++ train.py
@@ -15,22 +15,22 @@ valid_path = 'data/valid'
 train_batches = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(
     train_path,
     target_size=(224, 224),
-    batch_size=2
+    batch_size=12
 )
 
 valid_batches = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(
     train_path,
     target_size=(224, 224),
-    batch_size=2
+    batch_size=4
 )
 
-mobilenet_model = MobileNet(alpha=0.25)
+mobilenet_model = MobileNet()
 # mobilenet_model.summary()
 
 
 # replace last 5 layers with 'predictions' layer
 x = mobilenet_model.layers[-6].output
-predictions = Dense(2, activation='softmax')(x)
+predictions = Dense(64, activation='softmax')(x)
 model = Model(inputs=mobilenet_model.input, outputs=predictions)
 # model.summary()
 
@@ -41,7 +41,7 @@ for layer in model.layers[: -5]:
 # compile model
 model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit_generator(train_batches, steps_per_epoch=19, validation_data=valid_batches,
-                    validation_steps=19, epochs=20, verbose=2)
+model.fit_generator(train_batches, steps_per_epoch=41, validation_data=valid_batches,
+                    validation_steps=41, epochs=30, verbose=2)
 
-model.save('saved_models/fruits_2_5_20.h5')
+model.save('saved_models/fruits_30.h5')
